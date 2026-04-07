3ddc62caa0c6e071f6481444f03e396dc0f3d915
Xiaonai <xiaonai.ns1@gmail.com>
2026-04-07 12:27:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
-import AlphaSpotlight from './components/AlphaSpotlight'
 import ForYou from './pages/ForYou'
 import Fast from './pages/Fast'
 import Discover from './pages/Discover'
@@ -12,22 +11,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* For You page with Spotlight inside */}
-        <Route path="/" element={
-          <>
-            <AlphaSpotlight />
-            <ForYou />
-          </>
-        } />
-
-        {/* Other pages without Spotlight */}
+        <Route path="/" element={<ForYou />} />
         <Route path="/fast" element={<Fast />} />
         <Route path="/discover" element={<Discover />} />
         <Route path="/library" element={<Library />} />
         <Route path="/me" element={<Me />} />
         <Route path="/search" element={<Search />} />
       </Routes>
-
       <Footer />
     </BrowserRouter>
   )
