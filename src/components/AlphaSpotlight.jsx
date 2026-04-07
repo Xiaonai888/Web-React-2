67d487480afe2441c7ca7d9b1ece617ea8390b37
Xiaonai <xiaonai.ns1@gmail.com>
2026-04-07 14:37:14 +0700

    Update AlphaSpotlight.jsx

--- src/components/AlphaSpotlight.jsx
+++ src/components/AlphaSpotlight.jsx
@@ -2,27 +2,36 @@ import { useEffect } from 'react';
 
 export default function AlphaSpotlight() {
   useEffect(() => {
-    if (window.Swiper) {
-      new window.Swiper('.alphaSwiper', {
-        slidesPerView: 1.1,
-        spaceBetween: 12,
-        centeredSlides: false,
-        loop: false,
-        pagination: {
-          el: '.alpha-pagination',
-          clickable: true,
-        },
-      });
+    const initSwiper = () => {
+      if (window.Swiper) {
+        new window.Swiper('.alphaSwiper', {
+          slidesPerView: 1.1,
+          spaceBetween: 12,
+          centeredSlides: false,
+          loop: false,
+          pagination: {
+            el: '.alpha-pagination',
+            clickable: true,
+          },
+        });
+      }
+    };
+
+    if (document.readyState === 'complete') {
+      initSwiper();
+    } else {
+      window.addEventListener('load', initSwiper);
+      return () => window.removeEventListener('load', initSwiper);
     }
   }, []);
 
   const spotlightData = [
-    { id: 1, title: "CEO's Secret Baby", img: "/assets/alpha-spotlight/pic1.jpg", tag: "NEW" },
-    { id: 2, title: "The Revenge", img: "/assets/alpha-spotlight/pic2.jpg", tag: "HOT" },
-    { id: 3, title: "Omega Dragon", img: "/assets/alpha-spotlight/pic3.jpg", tag: "TOP" },
-    { id: 4, title: "My Princess", img: "/assets/alpha-spotlight/pic4.jpg", tag: "NEW" },
-    { id: 5, title: "Hidden Love", img: "/assets/alpha-spotlight/pic5.jpg", tag: "HOT" },
-    { id: 6, title: "Royal Scheme", img: "/assets/alpha-spotlight/pic6.jpg", tag: "END" },
+    { id: 1, title: "CEO's Secret Baby", img: "assets/alpha-spotlight/pic1.jpg", tag: "NEW" },
+    { id: 2, title: "The Revenge", img: "assets/alpha-spotlight/pic2.jpg", tag: "HOT" },
+    { id: 3, title: "Omega Dragon", img: "assets/alpha-spotlight/pic3.jpg", tag: "TOP" },
+    { id: 4, title: "My Princess", img: "assets/alpha-spotlight/pic4.jpg", tag: "NEW" },
+    { id: 5, title: "Hidden Love", img: "assets/alpha-spotlight/pic5.jpg", tag: "HOT" },
+    { id: 6, title: "Royal Scheme", img: "assets/alpha-spotlight/pic6.jpg", tag: "END" },
   ];
 
   return (
