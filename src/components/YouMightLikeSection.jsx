39197efd1207e6f385ed78c7acf0786bebc9dbcc
Xiaonai <xiaonai.ns1@gmail.com>
2026-04-13 12:31:10 +0700

    Update YouMightLikeSection.jsx

--- src/components/YouMightLikeSection.jsx
+++ src/components/YouMightLikeSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { youMightLikeBooks } from '../../Demo/YouMightLikeDemoPage'
+
 
 function BookCard({ book }) {
   return (
