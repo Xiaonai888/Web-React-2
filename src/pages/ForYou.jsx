b0bdbad086a9524d0aa7d62a174a3727aac4ca17
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-07 20:35:28 +0700

    Update ForYou.jsx

--- src/pages/ForYou.jsx
+++ src/pages/ForYou.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import ShadowSpotlight from '../components/ShadowSpotlight'
 import ShadowExclusiveSection from '../components/ShadowExclusiveSection'
