a9344abda5e39820f99d11c8c3bcfd2e292765ab
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-08 12:30:08 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import * as Sentry from '@sentry/react'
 import App from './App.jsx'
-
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  environment: import.meta.env.MODE,
-  tracesSampleRate: 0.1,
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
