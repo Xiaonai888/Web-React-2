07263c5cad6ec1bbb84915a50aa2ab5c0f987af6
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-17 15:05:20 +0700

    Update StoryHeroSection.jsx

--- src/components/story-detail/StoryHeroSection.jsx
+++ src/components/story-detail/StoryHeroSection.jsx
@@ -164,7 +164,9 @@ export default function StoryHeroSection({ story, onBack, bookmarked, onToggleBo
                     onClick={handleEcho}
                     className="flex w-full items-center gap-3 px-4 py-3 text-left text-[13px] font-extrabold hover:bg-[#f5f3fa]"
                   >
-                    <i className="fa-solid fa-retweet w-5 scale-x-125 text-[#8d94a1]" />
+                    <span className="flex h-6 w-6 items-center justify-center rounded-full bg-[#f2f4f7] text-[#667085]">
+  <i className="fa-solid fa-retweet scale-x-125 text-[13px]" />
+</span>
                     Echo
                   </button>
                 </div>
