cb6591c0d5351a7b0c36a01512f97fef52ca7bac
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-11 15:14:13 +0700

    Create ShadowMallPurchasePage.jsx

---
+++ src/pages/Shop/ShadowMallPurchasePage.jsx
@@ -0,0 +1,9 @@
+import PurchaseSection from '../../components/Shop/PurchaseSection'
+
+export default function ShadowMallPurchasePage() {
+  return (
+    <div className="min-h-screen bg-white px-4 pt-4">
+      <PurchaseSection />
+    </div>
+  )
+}
