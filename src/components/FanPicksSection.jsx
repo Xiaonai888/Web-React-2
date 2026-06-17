a7a5a3f09da1f424d8dc481f21563edfe23e42de
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-17 16:16:26 +0700

    Update grid layout in FanPicksSection component

--- src/components/FanPicksSection.jsx
+++ src/components/FanPicksSection.jsx
@@ -170,7 +170,7 @@ export default function FanPicksSection() {
         </h2>
       </div>
 
-      <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-5 lg:gap-4">
+      <div className="grid grid-cols-3 gap-x-4 gap-y-6 lg:grid-cols-5">
         {books.map((book) => (
           <button
             key={book.id}
