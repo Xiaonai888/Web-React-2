7fd5820ae2ba32c7eb7d2702c090f1de69442960
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-17 22:11:07 +0700

    Update ForYou.jsx

--- src/pages/ForYou.jsx
+++ src/pages/ForYou.jsx
@@ -686,12 +686,7 @@ export default function ForYou() {
   {[
     { icon: 'fa-shopping-bag', label: 'Shop', path: '/shop' },
     { icon: 'fa-tasks', label: 'Tasks', path: '/tasks' },
-    {
-      icon: 'fa-trophy',
-      label: 'Ranking',
-      path: '/ranking',
-      highlight: true,
-    },
+    { icon: 'fa-trophy', label: 'Ranking', path: '/ranking' },
     { icon: 'fa-calendar', label: 'Event', path: '/event' },
   ].map((item) => (
     <div
@@ -699,16 +694,8 @@ export default function ForYou() {
       className="group cursor-pointer"
       onClick={() => item.path && navigate(item.path)}
     >
-      <div
-        className={`mx-auto mb-2 flex h-12 w-12 items-center justify-center rounded-[14px] transition-all duration-200 group-hover:-translate-y-0.5 group-hover:bg-[#F6B800] ${
-          item.highlight
-            ? 'bg-[#F6B800]'
-            : 'bg-[#FFF4BF] ring-1 ring-[#F6B800]/30'
-        }`}
-      >
-        <i
-          className={`fas ${item.icon} text-[17px] text-[#111827]`}
-        />
+      <div className="mx-auto mb-1 flex h-12 w-12 items-center justify-center transition-all">
+        <i className={`fas ${item.icon} text-[#6b7280]`} />
       </div>
 
       <span className="text-[10px] font-semibold text-[#111827]">
