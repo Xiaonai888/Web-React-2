34fb1021943e15eae1b24e0ac50847d19dedb6e3
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-21 19:48:29 +0700

    Update EventPage.jsx

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom'.
+import { useNavigate } from 'react-router-dom'
 import { addStoryLanguageParam } from '../utils/storyLanguage'
 
 const API_BASE_URL = 'https://shadow-backend-kucw.onrender.com'
