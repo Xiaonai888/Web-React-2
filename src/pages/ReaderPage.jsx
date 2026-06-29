c65e3875daf6cf96c3efe8511c1dcab668b94cb7
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-29 22:33:56 +0700

    Update ReaderPage.jsx

--- src/pages/ReaderPage.jsx
+++ src/pages/ReaderPage.jsx
@@ -758,7 +758,7 @@ const [showAutoHint, setShowAutoHint] = useState(false)
         >
           <span className="flex h-16 w-16 shrink-0 items-center justify-center overflow-hidden rounded-[10px]">
             <img
-              src="/assets/Icons/Diamond%20box%202.png?v=2"
+              src="/assets/Task%20Center/Chest/chest-open.png?v=test1"
               alt=""
               className="h-full w-full object-contain"
               loading="eager"
