60714946c88fd55a89be9c5de8f75a8d53d619ea
Xiaonai <xiaonai.ns1@gmail.com>
2026-07-11 08:00:24 +0700

    Update ShadowMallSection.jsx

--- src/components/Shop/ShadowMallSection.jsx
+++ src/components/Shop/ShadowMallSection.jsx
@@ -1,4 +1,3 @@
-const mallShortcuts = [
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { addShadowMallCartItem } from '../../utils/shadowMallCart'
