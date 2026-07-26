2d5cc01a66f3e1739040183c8878132f29a5863f
Xiaonai <xiaonai.ns1@gmail.com>
2026-07-26 10:26:45 +0700

    Update TrendingNowSection.jsx

--- src/components/TrendingNowSection.jsx
+++ src/components/TrendingNowSection.jsx
@@ -51,7 +51,7 @@ function TrendingBookCard({ book, onOpen }) {
         />
 
         {book.isAdult ? (
-          <div className="absolute bottom-2 left-2 rounded-full bg-[#fff1f1] px-2.5 py-1 text-[10px] font-extrabold text-[#e5484d]">
+          <div className="absolute bottom-2 left-2 rounded-full bg-[#e5484d] px-2.5 py-1 text-[10px] font-extrabold text-white">
             18+
           </div>
         ) : null}
