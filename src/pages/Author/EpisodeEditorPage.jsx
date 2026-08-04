1651da8d11a6c4b7952320f048d06224fb7be3b4
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-04 19:36:15 +0700

    Update EpisodeEditorPage.jsx

--- src/pages/Author/EpisodeEditorPage.jsx
+++ src/pages/Author/EpisodeEditorPage.jsx
@@ -14,6 +14,7 @@ import {
 } from 'lucide-react'
 import RichFindReplacePanel from '../../components/Author/RichFindReplacePanel'
 import ImageDropZone from '../../components/common/ImageDropZone'
+import ScheduleReleasePicker from '../../components/author/ScheduleReleasePicker'
 import {
   MANGA_MAX_FILES_PER_PICK,
   MANGA_MAX_PAGES,
