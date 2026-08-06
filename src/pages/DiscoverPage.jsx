778ffa635763ebdfd1a6e991fc937a34eb71f8fe
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-07 00:08:38 +0700

    Update DiscoverPage.jsx

--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -351,6 +351,7 @@ function Header({ hidden }) {
 
           <Link
   to="/chat"
+  state={{ hideReaderFooter: true }}
   className="flex h-6 w-6 items-center justify-center text-[#111827] transition-transform active:scale-95"
   aria-label="Chat"
 >
