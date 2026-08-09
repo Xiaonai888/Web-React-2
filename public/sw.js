5eec4ea1c9cd5720477298a153ae5fa5ce1ae0ae
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-10 00:16:20 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,31 @@
+const RECOVERY_VERSION = '20260810-1'
+
 self.addEventListener('install', () => self.skipWaiting())
 
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()))
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    await self.clients.claim()
+    const clients = await self.clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true,
+    })
+
+    await Promise.all(clients.map((client) => {
+      const url = new URL(client.url)
+
+      if (url.origin !== self.location.origin) {
+        return Promise.resolve()
+      }
+
+      if (url.searchParams.get('__shadow_recover') === RECOVERY_VERSION) {
+        return Promise.resolve()
+      }
+
+      url.searchParams.set('__shadow_recover', RECOVERY_VERSION)
+      return client.navigate(url.href)
+    }))
+  })())
+})
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request))
