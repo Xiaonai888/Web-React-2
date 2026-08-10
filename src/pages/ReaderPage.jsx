738e8eb564926099826639c9aa89062586740fe0
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-10 22:59:32 +0700

    Update ReaderPage.jsx

--- src/pages/ReaderPage.jsx
+++ src/pages/ReaderPage.jsx
@@ -6220,7 +6220,8 @@ autoScrollEnabled ? (
 }
 
 navigate(`/story/${storyId}`, { replace: true })
-      className={lockedHeaderActive ? '!text-white' : 'text-[#111827]'}
+}}
+className={lockedHeaderActive ? '!text-white' : 'text-[#111827]'}
     />
 
     <div className="min-w-0 flex-1 px-3 text-center">
