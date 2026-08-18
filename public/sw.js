7a957c6b6ec9c3ba6d67d2034bbfc2cfb762ac5c
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-18 13:06:31 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,31 @@
-const RECOVERY_VERSION = '20260810-1'
+const RECOVERY_VERSION = '20260818-1'
 
 self.addEventListener('install', () => self.skipWaiting())
 
 self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
     await self.clients.claim()
+
+    const cacheNames = await caches.keys()
+    await Promise.all(cacheNames.map((name) => caches.delete(name)))
+
     const clients = await self.clients.matchAll({
       type: 'window',
       includeUncontrolled: true,
     })
 
+    await self.registration.unregister()
+
     await Promise.all(clients.map((client) => {
       const url = new URL(client.url)
 
-      if (url.origin !== self.location.origin) {
+      if (url.origin !== self.location.origin) return Promise.resolve()
+
+      if (url.searchParams.get('__shadow_sw_reset') === RECOVERY_VERSION) {
         return Promise.resolve()
       }
 
-      if (url.searchParams.get('__shadow_recover') === RECOVERY_VERSION) {
-        return Promise.resolve()
-      }
-
-      url.searchParams.set('__shadow_recover', RECOVERY_VERSION)
+      url.searchParams.set('__shadow_sw_reset', RECOVERY_VERSION)
       return client.navigate(url.href)
     }))
   })())
