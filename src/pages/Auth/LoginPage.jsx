0caebe840567c7aafe31d455a7877a1dc64bffba
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-02 14:25:28 +0700

    Update LoginPage.jsx

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -105,7 +105,7 @@ export default function LoginPage() {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [rememberMe, setRememberMe] = useState(false)
+  const [rememberMe, setRememberMe] = useState(true)
 
   const [showPassword, setShowPassword] = useState(false)
   const [loading, setLoading] = useState(false)
