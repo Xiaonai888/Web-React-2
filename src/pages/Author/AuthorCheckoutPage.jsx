5b73ef8d564f1c34db32ccce40de99ecbcbe2e28
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-10 22:27:09 +0700

    Update AuthorCheckoutPage.jsx

--- src/pages/Author/AuthorCheckoutPage.jsx
+++ src/pages/Author/AuthorCheckoutPage.jsx
@@ -1,7 +1,8 @@
-import { useMemo, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useMemo, useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { getDisplayLanguageId, useDisplayTranslation } from '../../utils/displayLanguage'
 import { registerTranslationNamespace } from '../../i18n/registerTranslations'
+import { getAuthorSellerKey, getSavedReaderMallSellerKey, setReaderMallActiveSellerKey } from '../../utils/readerMallCartHub'
 
 registerTranslationNamespace('authorCheckout', {
   "en": {
