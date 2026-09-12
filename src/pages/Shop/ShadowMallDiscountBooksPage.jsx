b5fb369801b937e4b9afede05ca74a3e3cc0b658
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-12 18:56:43 +0700

    Update ShadowMallDiscountBooksPage.jsx

--- src/pages/Shop/ShadowMallDiscountBooksPage.jsx
+++ src/pages/Shop/ShadowMallDiscountBooksPage.jsx
@@ -405,7 +405,11 @@ export default function ShadowMallDiscountBooksPage() {
               <ProductCard
                 key={product.id}
                 product={product}
-                onOpen={() => navigate(`/shop/mall/product/${product.id}`)}
+                onOpen={() =>
+  navigate(`/shop/mall/product/${product.id}`, {
+    state: { from: location.pathname + location.search + location.hash },
+  })
+}
               />
             ))}
           </section>
