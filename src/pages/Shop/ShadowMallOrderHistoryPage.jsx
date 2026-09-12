b8afc5e4216121c8d4a1ac95bdc3102efcf0b9bf
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-12 19:28:51 +0700

    Update ShadowMallOrderHistoryPage.jsx

--- src/pages/Shop/ShadowMallOrderHistoryPage.jsx
+++ src/pages/Shop/ShadowMallOrderHistoryPage.jsx
@@ -366,7 +366,10 @@ export default function ShadowMallOrderHistoryPage() {
             </p>
             <button
               type="button"
-              onClick={() => navigate('/shop')}
+              onClick={() => {
+  if (location.state?.from) return navigate(-1)
+  navigate('/shop', { replace: true })
+}}
               className="mt-5 rounded-full bg-[#111827] px-5 py-3 text-[13px] font-extrabold text-white active:scale-95 dark:bg-white dark:text-[#111827]"
             >
               Back to Shop
