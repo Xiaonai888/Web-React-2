ebecc291f72c60d90df0ba87ff72866c84f155d7
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-15 20:09:36 +0700

    Update DiscoverStorySection.jsx

--- src/components/discover/DiscoverStorySection.jsx
+++ src/components/discover/DiscoverStorySection.jsx
@@ -280,7 +280,7 @@ function StoryCard({
             src={
               latestStory.media_url
             }
-            alt={story?.alt_text || ''}
+            alt={latestStory?.alt_text || ''}
             loading="lazy"
             className="absolute inset-0 h-full w-full object-cover"
           />
@@ -733,7 +733,7 @@ function StoryViewer({
               <img
                 key={story.id}
                 src={story.media_url}
-                alt={story?.alt_text || ''}
+                alt={latestStory?.alt_text || ''}
                 className="relative h-full w-full object-contain"
               />
             </>
