43de6fccef79868cedbc9cacdc5b75d6abf7ed43
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-21 00:03:06 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1771,13 +1771,14 @@ self.addEventListener(
   return
 }
 
-    if (
-  /iPhone|iPad|iPod/.test(self.navigator.userAgent) &&
-  url.origin !== self.location.origin &&
-  url.pathname.startsWith('/api/')
-) {
-  return
-}
+       if (
+      (/iPhone|iPad|iPod/.test(self.navigator.userAgent) ||
+        iosDiagnosticClients.has(event.clientId)) &&
+      url.origin !== self.location.origin &&
+      url.pathname.startsWith('/api/')
+    ) {
+      return
+    }
 
     const episodeRoute =
       parseEpisodeApiUrl(url)
