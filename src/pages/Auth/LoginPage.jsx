49a0a212247276265acacd57e069874cca7bcb75
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-22 09:35:50 +0700

    Update LoginPage.jsx

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -141,6 +141,7 @@ export default function LoginPage() {
       if (data.deviceKey) localStorage.setItem('shadow_reader_device_key', data.deviceKey)
 
       const storage = rememberMe ? localStorage : sessionStorage
+            storage.setItem('shadow_reader_token', data.token)
       storage.setItem('shadow_reader_user', JSON.stringify(data.user))
 
       if (rememberMe) {
